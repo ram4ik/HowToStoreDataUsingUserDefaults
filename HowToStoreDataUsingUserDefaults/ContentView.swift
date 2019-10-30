359dbf14175ca6f782e84f3d81bd2b57eefe946a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-30 13:49:06 +0200

    init

--- HowToStoreDataUsingUserDefaults/ContentView.swift
+++ HowToStoreDataUsingUserDefaults/ContentView.swift
@@ -9,8 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var msg = ""
+    @State var retrieved = ""
     var body: some View {
-        Text("Hello World")
+        
+        VStack {
+            Text(self.retrieved)
+                .fontWeight(.heavy)
+                .padding()
+            
+            TextField("Enter msg To Save", text: $msg)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(action: {
+                UserDefaults.standard.set(self.msg, forKey: "msg")
+                self.retrieved = self.msg
+                self.msg = ""
+            }) {
+                Text("Save").padding(15)
+            }.background(Color.orange)
+            .foregroundColor(.white)
+            .cornerRadius(9)
+        }
+        .onAppear {
+            // this will perform whenever this appears on the screen...
+            // like viewDidLoad() in UIKit...
+            // it's saving and retriving...
+            
+            guard let retrievedmsg = UserDefaults.standard.value(forKey: "msg") else { return }
+            self.retrieved = retrievedmsg as! String
+        }
     }
 }
 
